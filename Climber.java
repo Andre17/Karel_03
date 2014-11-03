869e3d8db0ebc97a30fe4f3e14148162e664bc5d
Andre17 <andrepurits@Andre-Puritss-MacBook-Pro-2.local>
2014-11-03 12:49:40 +0000

    Lab03

---
+++ Climber.java
@@ -0,0 +1,44 @@
+// André Purits (2/11/14)
+import edu.fcps.karel2.Robot;
+import edu.fcps.karel2.Display;
+   public class Climber extends Athlete		         
+   {
+      public Climber()
+      {
+         super();
+      }
+      public Climber(int x)
+      {
+         super(x, 1, Display.NORTH, 1);
+}
+public void climbUpRight()
+{
+move();
+turnRight();
+move();
+turnLeft();
+}
+public void climbUpLeft()
+{
+move();
+turnLeft();
+move();
+turnRight();
+}
+public void climbDownRight()
+{
+turnAround();
+move();
+turnLeft();
+move();
+turnLeft();
+}
+public void climbDownLeft()
+{
+turnAround();
+move();
+turnRight();
+move();
+turnRight();
+} 
+}
